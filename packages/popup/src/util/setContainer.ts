4c6e24e3a2ed2a685e93d58a268c8ae5991bf306
wangweiqiang <wangweiqiang@lensung.com>
2025-04-29 09:16:08 +0800

    feat: build mode 修改

--- packages/popup/src/util/setContainer.ts
+++ packages/popup/src/util/setContainer.ts
@@ -1,6 +1,5 @@
 export function setContainer(): void {
   const urlParams = new URLSearchParams(window.location.search);
   const container = urlParams.get('container') || 'ext';
-  console.log('wq', container);
   window.container = container as 'web' | 'ext';
 }
